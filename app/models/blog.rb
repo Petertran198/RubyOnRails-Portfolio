a925465aae5934cda2419fd87072dd1dfdcf0989
petertran198 <petertran198@gmail.com>
2019-02-10 14:58:17 -0500

    Implemented custom scope for recent blogs and hide draft post from non admin user

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,5 @@
 class Blog < ApplicationRecord
-  enum status: { Draft: 0, Published: 1 }
+  enum status: { draft: 0, published: 1 }
   extend FriendlyId
   friendly_id :title, use: :slugged
 
@@ -10,5 +10,9 @@ class Blog < ApplicationRecord
 
   def self.special_blogs
     all
+  end
+
+  def self.recent
+    order("updated_at DESC")
   end
 end
