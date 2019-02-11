db45bd083304c0e864215fb98af0c54465ad0150
petertran198 <petertran198@gmail.com>
2019-02-10 22:12:01 -0500

    Added final paragraph revision for about page

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,6 +2,8 @@ class Topic < ApplicationRecord
   validates_presence_of :title
 
   has_many :blogs
+
+  ##This is querying all the topics that have a blog associate with it 
   def self.with_blogs
     includes(:blogs).where.not(blogs: {id: nil})
   end
