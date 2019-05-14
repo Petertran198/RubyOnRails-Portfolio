0066c0a1d7771ec50de0f7c22538b3b078b4f8d4
petertran198 <petertran198@gmail.com>
2019-05-14 13:43:06 -0400

    Revised links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,9 +3,8 @@ module ApplicationHelper
        if !current_user.is_a?(GuestUser)   
            link_to "Logout", destroy_user_session_path, method: :delete, class: style 
        else 
-           (link_to "Register", new_user_registration_path, class: style) +
-           " ".html_safe + ##you have to wrap it in () and + to concatenate it because it is ruby code and when it renders it will only return one thing
-           (link_to "Login", new_user_session_path, class: style) 
+           (link_to "Register", new_user_registration_path, class: style) + tag(:br) + "  ".html_safe+##you have to wrap it in () and + to concatenate it because it is ruby code and when it renders it will only return one thing
+           (link_to " Login", new_user_session_path, class: style) 
        end    
   end
 
