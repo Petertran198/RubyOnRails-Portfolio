43d9ac02ea0da94588d6252d48e80d5ab82f0a08
petertran198 <petertran198@gmail.com>
2019-06-27 17:12:18 -0400

    Added redirect to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,12 @@ class CommentsController < ApplicationController
   def create
     #@comment is going to grab the current_user and build the comment .This is allowed because of the relationship between user and comment
     @comment = current_user.build(comment_params)
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to blogs_path}
+      else
+        format.html { }
+    end
   end
 
 
