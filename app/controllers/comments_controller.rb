ab0c224a786ca6cf5f1a9c4e1fffad6907a62307
petertran198 <petertran198@gmail.com>
2019-06-27 17:40:34 -0400

    Changed build to new in comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
     #@comment is going to grab the current_user and build the comment .This is allowed because of the relationship between user and comment
-    @comment = current_user.build(comment_params)
+    @comment = current_user.comments.new(comment_params)
     respond_to do |format|
       if @comment.save
         format.html { redirect_to blogs_path }
