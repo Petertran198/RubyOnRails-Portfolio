1bf3e4b84c4ec115db37f883664bd56bf69cf05c
petertran198 <petertran198@gmail.com>
2019-06-27 17:57:17 -0400

    Added blog to comments relationship by controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,7 +14,7 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit(:content)
+    params.require(:comment).permit(:content, :blog_id, :user_id)
   end
 
 end
