0f109cb4aea33fa1a3cf5bc3bb667ce114ce6365
petertran198 <petertran198@gmail.com>
2019-06-27 17:47:58 -0400

    fixed .save in comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
     #@comment is going to grab the current_user and build the comment .This is allowed because of the relationship between user and comment
     @comment = current_user.comments.new(comment_params)
     respond_to do |format|
-      if @comment.save
+      if @comment.save!
         format.html { redirect_to blogs_path }
       else
         format.html { redirect_to root_path }
