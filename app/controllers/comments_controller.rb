dafea654a24ae70ad57aed2581770a6de00d1f97
petertran198 <petertran198@gmail.com>
2019-06-27 18:07:35 -0400

    fix blog connection from comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,9 +2,10 @@ class CommentsController < ApplicationController
   def create
     #@comment is going to grab the current_user and build the comment .This is allowed because of the relationship between user and comment
     @comment = current_user.comments.new(comment_params)
+    @blog = Blog.find(params[:blog_id])
     respond_to do |format|
       if @comment.save!
-        format.html { redirect_to blog_path(params[:blog_id]) }
+        format.html { redirect_to blog_path(@blog) }
       else
         format.html { redirect_to blogs_path }
       end
