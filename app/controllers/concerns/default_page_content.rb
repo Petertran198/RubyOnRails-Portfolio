18b8e6102f29c236f07afdc65318801cf330e3b1
Peter Tran <peter@adlabs.live>
2019-09-07 15:24:20 -0400

    Updated seo keywords

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -7,7 +7,7 @@ module DefaultPageContent
   def set_page_defaults
     @page_title = "Web Developer who specializes in Ruby on Rails, JavaScript, React , PHP, SCSS, PhotoShop among many other web programs or web applications. I am always looking additional jobs or collaborations.| Peter Tran "  
     @seo_keywords = "Ruby On Rails, Web Developer, JavaScript,React Js, Hiring,For hire, web design,responsive web design,website creator,graphic design"
-    @seo_description = "Local Web Developer who is always looking for additional programing jobs or opportunities."
+    @seo_description = "Local Full Stack Developer with industry experience building websites and web applications. I specialize in JavaScript and have professional experience working with Ruby on Rails, PHP, and WordPress."
   end
 
 end
